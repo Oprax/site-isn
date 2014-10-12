0f4a97c94ba0e039276185661a19ba3b144468b0
Oprax <oprax@me.com>
2014-10-12 22:07:01 +0200

    Fix CV

--- curriculum-vitae.php
+++ curriculum-vitae.php
@@ -10,7 +10,7 @@ include('includes/header.php');
 						- Titulaire de l'ASSR (Attestation Scolaire de Sécurité Routière) de niveau 2 depuis mai 2012.
 					</p>
 
-					<h2>Expèrience :</h2>
+					<h2>Expérience :</h2>
 					<p>&Agrave; travailler pendant trois semaines, du 4 au 22 août 2014, en tant que ouvrier agricole à la <a href="http://lafermemaurer.com/">Ferme Maurer</a>.</p>
 
 <?php
