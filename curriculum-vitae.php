9b24f7915acc6fd85b18166ea9444b64bb734a70
Oprax <oprax@me.com>
2014-10-12 21:24:35 +0200

    update CV

--- curriculum-vitae.php
+++ curriculum-vitae.php
@@ -4,12 +4,14 @@ include('includes/header.php');
 ?>
 					<h1>Curriculum vitae</h1>
 					
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+					<h2>Diplôme :</h2>
+					<p>
+						- Titulaire depuis 2012 du Brevet des Collèges avec la mention Bien.<br>
+						- Titulaire de l'ASSR (Attestation Scolaire de Sécurité Routière) de niveau 2 depuis mai 2012.
+					</p>
+
+					<h2>Expèrience :</h2>
+					<p>&Agrave; travailler pendant trois semaines, du 4 au 22 août 2014, en tant que ouvrier agricole à la <a href="http://lafermemaurer.com/">EARL Maurer</a>.</p>
 
 <?php
 include('includes/footer.php');
