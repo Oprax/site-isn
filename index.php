f36a359ec5948d42391fcfb8dd38be5cc7bef476
Oprax <oprax@me.com>
2014-10-13 07:15:43 +0200

    Update *.php file

--- index.php
+++ index.php
@@ -6,7 +6,8 @@ include('includes/header.php');
 					<h1>Accueil</h1>
 					
 					<p>
-						Bienvenue sur le site de la spécialité ISN (Informatique et Science du Numérique).
+						Bienvenue sur le site de la spécialité ISN (Informatique et Science du Numérique) de Muller Romain.<br>
+						Nous auront au cours de l'année scolaire le développement d'un projet qu'on présentera pour le bac.
 					</p>
 
 <?php
