ffa4228901c3f761a6ec8ee6b834d71060e302f0
Oprax <oprax@me.com>
2015-04-17 09:17:36 +0200

    Ajout du fichier carnet.php

---
+++ carnet.php
@@ -0,0 +1,28 @@
+<?php
+$title = 'Carnet de bord'; // For header
+include('includes/header.php');
+?>
+					<h1>Carnet de bord</h1>
+
+					<h2>Début du projet :</h2>
+					<p>
+						En décembre, Yann travaillait déja sur le projet avant que j'arrive, ainsi la fénètre PyGame et une partie de l'interface était déjà faite.
+						C'est-à-dire que que l'on pouvait déja dessiner, gommer et changer la couleur.
+						Pendant les vacances de noëls j'ai lu code du projet afin de renter dans le projet.
+					</p>
+
+					<h2>Avancement :</h2>
+					<p>
+						En janvier on a commencé a ce répartir les tâches.
+						Yann s'est occupé de l'import d'image, la rotation des images et la possibilté de changé la taille de la fenêtre.
+						Moi, je me suis occupé du retopur en arrière ainsi que de la sauvegarde de l'image.
+					</p>
+
+					<h2>Finalisation :</h2>
+					<p>
+						En Mars toute nos fonctionalités sont finis et opérationnelle. En avril nous entammons le refactoring ainsi que l'ajout et l'amélioration de commentaire dans le projet.
+					</p>
+
+<?php
+include('includes/footer.php');
+?>
